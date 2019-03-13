19fa2e0f1c354dfb52352c358d2b1ff4b4210b2b
Brit Armentrout <brittney.ann.armentrout@gmail.com>
2019-03-13 09:55:15 -0600

    added JawsDB to connection.js, created seeds with dummy data

---
+++ db/seeds.sql
@@ -0,0 +1,18 @@
+USE berm_db;
+
+INSERT INTO listings (item_name, item_price, item_category, item_image, seller_email, sold)
+VALUES ("2014 Kona Precept, Medium", 625.00, "bikes", "http://2014.konaworld.com/precept.cfm", "brittney.ann.armentrout@gmail.com");
+
+INSERT INTO listings (item_name, item_price, item_category, item_image, seller_email, sold)
+VALUES ("Five Ten Freerider Shoe - New, Size 8", 85.00, "apparel", "https://www.adidasoutdoor.com/five-ten-freerider-womens-mountain-bike-shoe/BC0785-10.html?gclid=EAIaIQobChMI3Imika__4AIVjLrACh3mqAGyEAQYASABEgIvdPD_BwE", "etobinkelsey@gmail.com");
+
+INSERT INTO listings (item_name, item_price, item_category, item_image, seller_email, sold)
+VALUES ("Crank Brothers Stamp 7 Pedals - Purple, used", 45.00, "parts", "https://express.google.com/u/0/product/4497327419238773613_4484773644062637986_9273173?ved=0CEcQjsUCKARqGAoTCM2Fppax_-ACFQdjAQodjpYBABCAAQ", "mtnbike4life@gmail.com");
+
+INSERT INTO listings (item_name, item_price, item_category, item_image, seller_email, sold)
+VALUES ("2015 Yeti Beti, Small", 1899.00, "bikes", "https://www.pinkbike.com/photo/16348093/", "coloradoDHgirl@yahoo.com");
+
+INSERT INTO listings (item_name, item_price, item_category, item_image, seller_email, sold)
+VALUES ("Helmet - Troy Lee A2 MIPS, size M, slightly used", 75.00, "accessories", "https://www.rei.com/product/129619/troy-lee-designs-a2-decoy-mips-bike-helmet?CAWELAID=120217890005062641&CAGPSPN=pla&CAAGID=15877514200&CATCI=pla-348196920197&cm_mmc=PLA_Google|404_33902|1296190014|none|a23a8dde-e319-4754-a277-932311ff79b1|pla-348196920197&lsft=cm_mmc:PLA_Google_LIA|404_33902|1296190014|none|a23a8dde-e319-4754-a277-932311ff79b1&kclid=a23a8dde-e319-4754-a277-932311ff79b1&gclid=EAIaIQobChMIhOTK27T_4AIVBBxpCh0fFAeyEAQYASABEgIJBvD_BwE", "milkshakes@aol.com");
+
+
