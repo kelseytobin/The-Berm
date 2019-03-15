50663d869167388593fa8a7f78bff9da2d15f6ce
Kelsey Tobin <etobinkelsey@gmail.com>
2019-03-14 20:04:30 -0600

    developed model and approved server connection

--- models/listings.js
+++ models/listings.js
@@ -3,20 +3,65 @@
 // =============================================================
 
 // Sequelize (capital) references the standard library
-var Sequelize = require("sequelize");
+// var Sequelize = require("sequelize");
 // sequelize (lowercase) references our connection to the DB.
-var sequelize = require("../config/connection.js");
+// var sequelize = require("../config/connection.js");
 
-var Listings = sequelize.define("listing", {
-  item_name: Sequelize.STRING,
-  item_price: Sequelize.INTEGER,
-  item_category: Sequelize.STRING,
-  // item_image: not sure about this one
-  seller_email: Sequelize.STRING,
-  createdAt: Sequelize.STRING,
-  sold: Sequelize.DataTypeBoolean
-});
+// var Listings = sequelize.define("listing", {
+//   item_name: Sequelize.STRING,
+//   item_price: Sequelize.INTEGER,
+//   item_category: Sequelize.STRING,
+//   // item_image: not sure about this one
+//   seller_email: Sequelize.STRING,
+//   createdAt: Sequelize.STRING,
+//   sold: Sequelize.DataTypeBoolean
+// });
 
-Listings.sync();
+// Listings.sync();
 
-module.exports = Listings;
+// module.exports = Listings;
+
+module.exports = function(sequelize, DataTypes) {
+  var Listings = sequelize.define("Listings", {
+    item_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    item_price: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    item_category: {
+      type: DataTypes.STRING,
+      defaultValue: "Bikes"
+    },
+    seller_email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    createdAt: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    sold: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    }
+  });
+  return Listings;
+};
--- server.js
+++ server.js
@@ -12,6 +12,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static("public"));
 
+// Requiring our models for syncing
+var db = require("./models");
+
 // Handlebars
 app.engine(
   "handlebars",
@@ -22,8 +25,8 @@ app.engine(
 app.set("view engine", "handlebars");
 
 // Routes
-// require("./routes/apiRoutes")(app);
-// require("./routes/htmlRoutes")(app);
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
 var syncOptions = { force: false };
 
@@ -34,22 +37,14 @@ if (process.env.NODE_ENV === "test") {
 }
 
 // Starting the server, syncing our models ------------------------------------/
-// db.sequelize.sync(syncOptions).then(function() {
-//   app.listen(PORT, function() {
-//     console.log(
-//       "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
-//       PORT,
-//       PORT
-//     );
-//   });
-// });
-
-//basic local host connection to ensure that is working, swtich back to sequelize connection when ready
-//=======================================================================================================
-
-app.listen(PORT, function() {
-  // Log (server-side) when our server has started
-  console.log("Server listening on: http://localhost:" + PORT);
+db.sequelize.sync(syncOptions).then(function() {
+  app.listen(PORT, function() {
+    console.log(
+      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+      PORT,
+      PORT
+    );
+  });
 });
 
 module.exports = app;
