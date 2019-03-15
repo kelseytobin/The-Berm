ec696a53d4752e6eaaef6af1688d2ba0fcfa411f
Kelsey Tobin <etobinkelsey@gmail.com>
2019-03-15 08:52:09 -0600

    uncommented routes files and added in our table name

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,28 +1,29 @@
-// var db = require("../models");
+var db = require("../models/index.js");
 
-// module.exports = function(app) {
-//   // Load index page
-//   app.get("/", function(req, res) {
-//     db.Example.findAll({}).then(function(dbExamples) {
-//       res.render("index", {
-//         msg: "Welcome!",
-//         examples: dbExamples
-//       });
-//     });
-//   });
+module.exports = function(app) {
+  // Load index page
+  app.get("/", function(req, res) {
+    db.Listings.findAll({}).then(function(dbListings) {
+      res.render("index", {
+        msg: "Welcome!",
+        examples: dbListings
+      });
+    });
+  });
 
+  // Load example page and pass in an example by id
+  app.get("/listings/:id", function(req, res) {
+    db.Listings.findOne({ where: { id: req.params.id } }).then(function(
+      dbListings
+    ) {
+      res.render("listings", {
+        example: dbListings
+      });
+    });
+  });
 
-//   // Load example page and pass in an example by id
-//   app.get("/example/:id", function(req, res) {
-//     db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-//       res.render("example", {
-//         example: dbExample
-//       });
-//     });
-//   });
-
-//   // Render 404 page for any unmatched routes
-//   app.get("*", function(req, res) {
-//     res.render("404");
-//   });
-
+  // Render 404 page for any unmatched routes
+  app.get("*", function(req, res) {
+    res.render("404");
+  });
+};
