99d954bdf32bf983a36ef43b8155c6a60461e96c
Kelsey Tobin <etobinkelsey@gmail.com>
2019-03-18 14:49:45 -0600

    api routes working except for view all, added additional hbs view pages, started on bikes.hbs, htmlroutes not working

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,29 +1,49 @@
 var db = require("../models/index.js");
 
+// Dependencies
+// =============================================================
+var path = require("path");
+
+// Routes
+// =============================================================
 module.exports = function(app) {
-  // Load index page
+
+  // Each of the below routes just handles the HTML page that the user gets sent to.
+
+  //DONT THINK ANY OF THIS IS RIGHT
+
+  // route loads home page
+  //trying this instead
   app.get("/", function(req, res) {
-    db.Listings.findAll({}).then(function(dbListings) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbListings
-      });
-    });
+    res.render("index");
+  
+  // app.get("/", function(req, res) {
+  //   res.sendFile(path.join(__dirname, "../views/layouts/main.handlebars"));
+  // });
+
+  //route loads display bike page
+  app.get("/bikes", function(req, res) {
+    res.sendFile(path.join(__dirname, "../views/layouts/bikes.handlebars"));
   });
 
-  // Load example page and pass in an example by id
-  app.get("/listings/:id", function(req, res) {
-    db.Listings.findOne({ where: { id: req.params.id } }).then(function(
-      dbListings
-    ) {
-      res.render("listings", {
-        example: dbListings
-      });
-    });
+  // route loads display accessories page
+  app.get("/blog", function(req, res) {
+    res.sendFile(path.join(__dirname, "../views/layouts/accessories.handlebars"));
+  });
+
+  // route loads display apparrel page
+  app.get("/blog", function(req, res) {
+    res.sendFile(path.join(__dirname, "../views/layouts/apparel.handlebars"));
+  });
+
+  // route loads display view all page
+  app.get("/blog", function(req, res) {
+    res.sendFile(path.join(__dirname, "../views/layouts/all.handlebars"));
   });
 
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
     res.render("404");
   });
-};
+});
+}
