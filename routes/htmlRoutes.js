6b8686e6021d055210c80a2ca76faaaf5f4fa38a
Brit Armentrout <brittney.ann.armentrout@gmail.com>
2019-03-19 10:31:45 -0600

    working hbs routes, delete example.hbs

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -14,23 +14,22 @@ module.exports = function(app) {
 
   //route loads display bikes page
   app.get("/bikes", function(req, res) {
-    res.sendFile(path.join(__dirname, "../views/bikes.handlebars"));
-    console.log("this is bikes page");
+    res.render(path.join(__dirname, "../views/bikes.handlebars"));
   });
 
   //route loads display accessories page
   app.get("/accessories", function(req, res) {
-    res.sendFile(path.join(__dirname, "../views/accessories.handlebars"));
+    res.render(path.join(__dirname, "../views/accessories.handlebars"));
   });
 
   //route loads display apparrel page
   app.get("/apparel", function(req, res) {
-    res.sendFile(path.join(__dirname, "../views/apparel.handlebars"));
+    res.render(path.join(__dirname, "../views/apparel.handlebars"));
   });
 
   // route loads display view all page
   app.get("/all", function(req, res) {
-    res.sendFile(path.join(__dirname, "../views/all.handlebars"));
+    res.render(path.join(__dirname, "../views/all.handlebars"));
   });
 
   // Render 404 page for any unmatched routes
