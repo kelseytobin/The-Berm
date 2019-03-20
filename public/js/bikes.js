f73f34c94e4930f06f919aaed7c84ed272f08602
Kelsey Tobin <etobinkelsey@gmail.com>
2019-03-19 21:37:27 -0600

    working hbs display, still need to change main.hbs

--- public/js/bikes.js
+++ public/js/bikes.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     function getBikes() {
-        $("#bikeBtn").on("click", function(data) {
+
           $.get("/api/bikes")
             .then(function (data) {
             
@@ -11,24 +11,25 @@ $(document).ready(function () {
               var listingBody = $("<div>");
               listingBody.addClass("uk-card");
               listingBody.attr("id", "listing" + i);
-              $(".listingBody").append(listingBody);
-    
+              $("#listingBody").append(listingBody);
+                
               //add listing data to card we just placed on the page
               // make the name an h2,
-              $(".listingBody" + i).append("<h2>" + data[i].item_name + "</h2>");
+              $("#listing" + i).append("<h2>" + data[i].item_name + "</h2>");
               // the price an h3,
-              $(".listingBody" + i).append("<h3>" + data[i].item_price + "</h3>");
+              $("#listing" + i).append("<h3>" + data[i].item_price + "</h3>");
               // the category an h3,
-              $(".listingBody" + i).append("<h3>" + data[i].item_category + "</h3>");
+              $("#listing" + i).append("<h3>" + data[i].item_category + "</h3>");
               // and the createdAt an h3.
-              $(".listingBody" + i).append("<h3>" + data[i].createdAt + "</h3>");
+              $("#listing" + i).append("<h3>" + data[i].createdAt + "</h3>");
+              console.log(listingBody);
             }
           })
           
           //test to see button is live
           console.log("button was clicked!");
          
-        });
+        
       }
 getBikes();
 });
